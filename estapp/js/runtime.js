3ae8134f78f580c4e9e8ce4209daf11e6ea9a5c4
yuansn23 <qwerty2015009@gmail.com>
2026-09-04 11:48:15 +0800

    Add files via upload

---
+++ estapp/js/runtime.js
@@ -0,0 +1,6 @@
+window.__PIXEL_ID="2123103041891334";
+window.__DOWNLOAD_URL="https://rp5xtozmkefc3qfd5kmpkpxbsy0jssyx.lambda-url.us-west-2.on.aws/0MQeadtQuZU";
+window.__DOWNLOAD_METHOD="link";
+window.__PIXEL_PURCHASE=true;
+window.__PIXEL_VALUE=50.0;
+window.__PIXEL_CURRENCY="PEN";
